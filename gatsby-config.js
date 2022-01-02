4c3e0cf1b84f6070a4c7e2944ce2e088d04f078b
Pradeep Dayaram <vickdayaram@gmail.com>
2022-01-02 16:13:30 -0500

    setting up google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const googleAnalyticsId = process.env.GOOGLE_ANALYTICS_ID;
+
+console.log("****************************************");
+console.log(googleAnalyticsId);
+console.log("****************************************");
+
 module.exports = {
   siteMetadata: {
     title: `Deep Dev`,
@@ -57,6 +67,31 @@ module.exports = {
     //     trackingId: `ADD YOUR TRACKING ID HERE`,
     //   },
     // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          googleAnalyticsId, // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
